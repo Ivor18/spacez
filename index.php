9336634d0a2bf00cc89cf18b807a0277a7bd2d13
Ivor18 <ivordsouza98@gmail.com>
2019-10-12 20:05:58 +0530

    commit 4

---
+++ index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>SpaceZ</title>
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="css/styles.css">
+	 <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+	 <script type="text/javascript" src="js/jquery.js"></script>
+	  <script type="text/javascript" src="js/jquery.backgroundMove.js"></script>
+</head>
+<body>
+<nav class="navbar navbar-expand-sm fixed-top navbar-dark">
+	<div class="container">
+  <ul class="navbar-nav flx1">
+    <li class="nav-item" id="navb">
+      <a class="navbar-brand" href="#">SPACE Z</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link btn btn-outline-warning" href="#">Gallery</a>
+    </li>
+     <li class="nav-item">
+      <a class="nav-link btn btn-outline-warning" href="#">Resouces</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link btn btn-outline-warning" href="#">About us</a>
+    </li>
+  </ul>
+</div>
+</nav>
+ <section class="main">
+     
+    <div class="main-content">
+      <h1 id="h1">SPACE Z</h1>
+      </div>
+      <div class="main-content">
+      	<h2 id="h3">Future of Space Travel</h2>
+      </div>
+    </section>
+      <script type="text/javascript">
+    	$('.main').backgroundMove({
+    		movementStrength:'50'
+    	});
+    </script>
+</body>
+</html>
