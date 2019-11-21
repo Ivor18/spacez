365163f9062e8f222e1574443dd6e925c48700a1
Ivor18 <ivordsouza98@gmail.com>
2019-11-21 23:55:16 +0530

    homepage change

--- index.php
+++ index.php
@@ -14,22 +14,22 @@
   <div class="container">
   <ul class="navbar-nav flx1">
     <li class="nav-item" id="navb">
-      <a class="navbar-brand" href="#">SPACE Z</a>
+      <a class="navbar-brand" href="#"> <img src="img/White.png"></a>
     </li>
     <li class="nav-item">
-      <a class="nav-link btn btn-outline-warning" href="gallery.html">Gallery</a>
+      <a class="nav-link btn btn-outline-danger" href="gallery.html">Gallery</a>
     </li>
      <li class="nav-item">
       <a class="nav-link btn btn-outline-warning" href="choose.html">Book a Seat</a>
     </li>
     <li class="nav-item">
-      <a class="nav-link btn btn-outline-warning" href="#">About us</a>
+      <a class="nav-link btn btn-outline-info" href="#">About us</a>
     </li>
   </ul>
 </div>
 </nav>
 <div class="bg"></div>
-   
+   <div class="logo"></div>
       <video autoplay muted loop id="myVideo">
   <source src="vvv.mp4" type="video/mp4">
     </video>
@@ -38,39 +38,66 @@
    
     <div class="main-content">
       <h1 id="h1">SPACE Z</h1>
+       
       </div>
       <div class="main-content">
         <h2 id="h3">Future of Space Travel</h2>
       </div>
  </section>
-    <div class="section">
-    <div class="item">
-      <h1>Booking</h1>
-      <h3>Details</h3>
-      
-    </div>
-     <div class="item">
-      <h1>Shop</h1>
-      <h3>Details</h3>
-      
-    </div>
-     <div class="item">
-      <h1>About</h1>
-      <h3>Details</h3>
-      
-    </div>
-</div>
+    <section class="section">
+<div id="content1" class="jumbotron fs">
+        <h1>Destinations</h1>
+    <p>Moon, Mars and beyond!!!</p>
+        </div>
+        <div id="content2" class="jumbotron fe">
+        <h1>Space vehicle</h1>
+    <p>Safe and comfortable journey</p>
+        </div>
+        <div id="content3" class="jumbotron fs">
+        <h1>Cost</h1>
+    <p>Affordable!!!</p>
+        </div>
+           <div id="content4" class="jumbotron fe">
+        <h1>Gallery Details</h1>
+    <p>View previous expeditions</p>
+        </div>
+     
+        
+   
+</section>
    <script type="text/javascript">
       // $('.bg').backgroundMove({
       //  movementStrength:'50'
       // });
       $(window).scroll(function(){
-        console.log($(window).scrollTop());
-    $(".bg")
-      .css("transform", "scale("+(1-$(window).scrollTop()*0.001)+") translateY("+(0.225*$(window).scrollTop())+"vh)");
       
-
+          if(1-$(window).scrollTop()*0.001>0.3){
+    $(".bg")
+      .css("transform", "scale("+(1-$(window).scrollTop()*0.001)+") translateY("+(0.350*$(window).scrollTop())+"vh)");
+          }
+          else{
+              $(".bg").css("transform", "scale(0.3) translateY("+(0.350*$(window).scrollTop())+"vh)");
+           $(".logo").css("display", "block")
+               $(".logo").css("transform", "scale(0.3) translateY("+(0.350*$(window).scrollTop())+"vh)");
+          }
+          console.log(1-$(window).scrollTop()*0.001)
 });
+       
+//       $(document).on("scroll", function () {
+//var pageTop = $(document).scrollTop()
+//var pageBottom = pageTop + $(window).height()
+//var tags = $("section")
+//
+//for (var i = 0; i < tags.length; i++) {
+//var tag = tags[i]
+//
+//if ($(tag).position().top < pageBottom) {
+//$(tag).addClass("visible")
+//} else {
+//  $(tag).removeClass("visible")
+//}
+//}
+//})
     </script>
 </body>
 </html>
